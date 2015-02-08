a522d8dd3822583807973ce9a115e24889776529
Mani bhushan <manibhushan.cs@gmail.com>
2015-02-08 06:29:33 +0530

    experimenting with fibonaccci

---
+++ chap0/fibonacci.py
@@ -0,0 +1,51 @@
+from readint import readinput
+import sys
+import math
+
+
+def findfibonacci(num):
+    if num <= 1:
+        return num
+    return findfibonacci(num - 1) + findfibonacci(num - 2)
+
+
+def findfibopoly(num):
+    if num <= 1:
+        return num
+    a = 0
+    b = 1
+    for n in range(num):
+        f = a + b
+        a = b
+        b = f
+    return f
+
+
+def findfibexpr(num):
+    if num <= 1:
+        return num
+    exp = 0.694 * num
+    fib = int(math.floor(math.pow(2, exp)))
+    return fib
+
+
+def main():
+    num = readinput()
+    for n in range(num+1):
+        fib = findfibonacci(n)
+        sys.stdout.write(str(fib) + " "),
+    sys.stdout.write("\n")
+    print ("Finding fibonacci through expression 2**(0.694*n)")
+    for n in range(num):
+        fib = findfibexpr(n)
+        sys.stdout.write(str(fib) + " "),
+    sys.stdout.write("\n")
+
+    print ("Enter number for fast fibonacci")
+    num = readinput()
+    fib = findfibopoly(num)
+    print ("fib %d is: %d" % (num, fib))
+
+
+if __name__ == '__main__':
+    main()
