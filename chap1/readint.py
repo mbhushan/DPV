182ad96852a8b5198e60e2ac3f36ae79739fae7e
Mani bhushan <manibhushan.cs@gmail.com>
2015-02-16 07:45:45 +0530

    reading int values from console

---
+++ chap1/readint.py
@@ -0,0 +1,21 @@
+def readinput(X="number"):
+    while True:
+        num = input("Enter %s: " % (X))
+        num = num.strip()
+        if not num.isdigit():
+            print ("Bad Input. Please try again..")
+            continue
+        num = int(num)
+        if num < 0:
+            print ("Bad Integer input - Please enter positive number")
+            continue
+        return num
+
+
+def main():
+    num = readinput()
+    print ("Input number:", num)
+
+
+if __name__ == '__main__':
+    main()
