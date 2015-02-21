bae55caaed31151c27e1fcd83edfa2507208c0e7
Mani bhushan <manibhushan.cs@gmail.com>
2015-02-21 12:55:35 +0530

    generate random prime less than n

---
+++ chap1/random_prime.py
@@ -0,0 +1,35 @@
+from readint import readinput
+from little_fermat import isprime
+import random
+
+
+def random_prime(num):
+    while True:
+        n = random.randint(2, num)
+        if isprime(n):
+            return n
+
+
+def test_random_prime():
+    pmap = {}
+    num = 100
+    for i in range(25000):
+        p = random_prime(num)
+        if p in pmap:
+            pmap[p] += 1
+        else:
+            pmap[p] = 1
+    print ("Total prime less than %d: %d" % (num, len(pmap)))
+    for p in pmap:
+        print("%d: %d" % (p, pmap[p]))
+
+
+def main():
+    num = readinput("n >= 2")
+    p = random_prime(num)
+    print ("Random Prime less than %d: %d" % (num, p))
+    test_random_prime()
+
+
+if __name__ == '__main__':
+    main()
