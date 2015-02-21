815b048802c1bc76f974a0fb7b9c92b300f29ba0
Mani bhushan <manibhushan.cs@gmail.com>
2015-02-21 12:55:09 +0530

    fixed edge case

--- chap1/little_fermat.py
+++ chap1/little_fermat.py
@@ -4,9 +4,13 @@ import random
 
 
 def isprime(p):
+    if p == 1:
+        return False
+    if p == 2:
+        return True
     n = min(p, 100)
     for i in range(n):
-        a = random.randint(1, p-1)
+        a = random.randint(2, p-1)
         lhs = modular_exp(a, p-1, p)
         if lhs != 1:
             return False
@@ -22,6 +26,10 @@ def main():
         print ("%d is Prime Number" % p)
     else:
         print ("%d is Not a Prime Number" % p)
+    for i in range(100000):
+        if isprime(4):
+            print("we are getting 4 as prime")
+    print ("Done!")
 
 
 if __name__ == '__main__':
