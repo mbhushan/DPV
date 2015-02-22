259bbf188d1ac95a971f857ad0fd7a5eef54b58a
Mani bhushan <manibhushan.cs@gmail.com>
2015-02-22 08:40:41 +0530

    modularized code

--- chap1/inverse_modulo.py
+++ chap1/inverse_modulo.py
@@ -2,15 +2,20 @@ from readint import readinput
 from extended_euclid import extended_euclid
 
 
+def inverse_modulo(x, n):
+    ans = extended_euclid(x, n)
+    if ans[0] > 0:
+        return ans[0]
+    else:
+        return (ans[0] + n)
+
+
 def main():
     print ("To calculate: X^(-1) mod N")
     x = readinput("X")
     n = readinput("N")
-    ans = extended_euclid(x, n)
-    if ans[0] > 0:
-        print ("Ans: ", ans[0])
-    else:
-        print ("Ans: ", (ans[0] + n))
+    ans = inverse_modulo(x, n)
+    print ("Ans: ", ans)
 
 
 if __name__ == '__main__':
