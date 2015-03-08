a945115b9e00a74791b9bc72c3585dc940bb1ffa
Mani bhushan <manibhushan.cs@gmail.com>
2015-03-08 09:53:05 +0530

    fixed cases where inverse does not exists

--- chap1/inverse_modulo.py
+++ chap1/inverse_modulo.py
@@ -1,8 +1,17 @@
 from readint import readinput
 from extended_euclid import extended_euclid
+from gcd import gcd
 
 
 def inverse_modulo(x, n):
+    a, b = x, n
+    if n > x:
+        a, b = b, a
+    result = gcd(a, b)
+    if result != 1:
+        print ("As gcd(%d, %d) != 1, the inverse does not exists" % (x, n))
+        return None
+
     ans = extended_euclid(x, n)
     if ans[0] > 0:
         return ans[0]
