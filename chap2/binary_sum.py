ffa74c7d9fe9e799394e611127fb2a6596f53787
manibhushan <manibhushan.cs@gmail.com>
2015-08-29 22:36:50 +0530

    binary sum of 2 strings

---
+++ chap2/binary_sum.py
@@ -0,0 +1,10 @@
+
+def binsum(x, y):
+    """ Assumes x and y are binary strings,
+    returns sum of x and y as binary string """
+
+def main():
+    binsum(x, y)
+
+if __name__ == '__main__':
+    main()
