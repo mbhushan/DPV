96c27dd88f0b3cf6d05b567970f39922c30df551
manibhushan <manibhushan.cs@gmail.com>
2015-08-30 20:48:59 +0530

    binary string input

--- chap2/binary_sum.py
+++ chap2/binary_sum.py
@@ -1,9 +1,27 @@
 
+def readinput(k):
+    bstr = raw_input("Enter a binary string %s: " % k)
+    return bstr
+
+
 def binsum(x, y):
     """ Assumes x and y are binary strings,
     returns sum of x and y as binary string """
+    xlen = len(x)
+    ylen = len(y)
+    diff = abs(xlen - ylen)
+    z = "0" * diff
+    if xlen > ylen:
+        y = z + y
+    elif ylen > xlen:
+        x = z + x
+    print "x: ", x
+    print "y: ", y
+
 
 def main():
+    x = readinput("x")
+    y = readinput("y")
     binsum(x, y)
 
 if __name__ == '__main__':
