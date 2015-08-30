f5e22288940597ff0e6f792bec6c099c35fa40e4
manibhushan <manibhushan.cs@gmail.com>
2015-08-30 21:38:18 +0530

    final binary sum logic

--- chap2/binary_sum.py
+++ chap2/binary_sum.py
@@ -4,6 +4,27 @@ def readinput(k):
     return bstr
 
 
+def bitsum(a, b, carry):
+    """ assumes a, b and carry are single bits
+    returns result = (sum of bits, carry) """
+    result = (0, 0)
+    c = a + b
+    if c == 1 and carry == 0:
+        result = 1, 0
+    elif c == 1 and carry == 1:
+        result = 0, 1
+    elif c == 2 and carry == 0:
+        result = 0, 1
+    elif c == 2 and carry == 1:
+        result = 1, 1
+    elif c == 0:
+        result = carry, 0
+    else:
+        print "sum: ", c, " carry: ", carry
+        print "ERR: Should not have come here!"
+    return result
+
+
 def binsum(x, y):
     """ Assumes x and y are binary strings,
     returns sum of x and y as binary string """
@@ -17,12 +38,25 @@ def binsum(x, y):
         x = z + x
     print "x: ", x
     print "y: ", y
+    result = []
+    carry = 0
+    for i in range(xlen-1, -1, -1):
+        # print "x, y, carry: ", x[i], " ", y[i], " ", carry
+        sm, carry = bitsum(int(x[i]), int(y[i]), carry)
+        # print "sum: ", sm, " carry: ", carry
+        result.append(sm)
+    if carry == 1:
+        result.append(carry)
+    result = result[::-1]
+    result = ''.join(str(r) for r in result)
+    return result
 
 
 def main():
     x = readinput("x")
     y = readinput("y")
-    binsum(x, y)
+    result = binsum(x, y)
+    print "Sum is: ", result
 
 if __name__ == '__main__':
     main()
